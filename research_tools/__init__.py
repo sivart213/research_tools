6e2a437d40df7256020ee9e8d9648d51bfcc72e2
sivart213 <78872903+sivart213@users.noreply.github.com>
2024-05-05 21:28:22 -0700

    restore init

--- research_tools/__init__.py
+++ research_tools/__init__.py
@@ -8,7 +8,8 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-""" from .equations.general import (
+
+from .equations.general import (
     arc,
     arccosd,
     arcsind,
@@ -398,4 +399,3 @@ __all__ = [
     "slugify",
 
 ]
- """
